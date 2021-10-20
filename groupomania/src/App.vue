bc281a234780aa41cc120f764f9f1274b53696b1
‘fabrice’ <fabrice.avrila@gmail.com>
2021-10-20 11:35:00 +0200

    Vue

--- groupomania/src/App.vue
+++ groupomania/src/App.vue
@@ -1,20 +1,23 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavLink from './components/NavLink.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		NavLink
+	}
 }
 </script>
+
+<template>
+	<div id="app">
+		<nav>
+			<NavLink url="/" text="Accueil" />
+			<NavLink url="/about" text="À propos" />
+			<NavLink url="/contact" text="Contact" />
+		</nav>
+	</div>
+</template>
 
 <style>
 #app {
